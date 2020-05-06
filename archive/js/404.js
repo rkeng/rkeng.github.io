5e9e65e0adc12715f245051b56dfab0f085de37a
Ryan Keng <rkeng@ucsd.edu>
2020-05-06 09:54:16 +0800

    New favicon, fix minor styles, remove archive

--- archive/js/404.js
+++
@@ -1,6 +0,0 @@
-$(".content-404").fadeIn(1000, function(){
-   // logo wobbles once after fadeIn animation completes
-   $('#logo').addClass('wobble-onload').delay(1000).queue(function(){
-      $(this).removeClass("wobble-onload").dequeue();
-   });
-});
